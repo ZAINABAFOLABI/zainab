8dad0bfa58b1f1279459ff34ec638dd112c6a3bc
ZAINABAFOLABI <zainab.o.afolabi@gmail.com>
2023-11-07 14:38:21 +0000

    skills & experience UI setup

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  angularLogo = '../assets/images/Angular_full_color_logo.svg.png';
+  pythonLogo = '../assets/images/python logo.svg';
+  javascriptLogo = '../assets/images/JavaScript-logo.png';
+  dotNetLogo = '../assets/images/Microsoft.net.png';
 
   constructor() { }
 
--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-portfolio',
+  selector: 'zainab-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.css']
 })
 export class PortfolioComponent implements OnInit {
+  alphaAppDB= '../assets/images/Alphaapp dashboard.png ';
+  salesUI = '../assets/images/SalesUI.png ';
 
   constructor() { }
 
