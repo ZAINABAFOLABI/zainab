8d79bcea288c5e5895b56c9fcdfa6942b6617be1
ZAINABAFOLABI <zainab.o.afolabi@gmail.com>
2023-11-16 11:19:26 +0000

    Expertise module UI set up

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class PortfolioComponent implements OnInit {
   alphaAppDB= '../assets/images/Alphaapp dashboard.png ';
   salesUI = '../assets/images/SalesUI.png ';
+  loginUIDesign = '../assets/images/Login UI design 1.png';
 
   constructor() { }
 
