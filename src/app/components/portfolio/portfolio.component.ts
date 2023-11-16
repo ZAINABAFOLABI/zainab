efa9a69823174254edc7e731139af601dafdc5ce
ZAINABAFOLABI <zainab.o.afolabi@gmail.com>
2023-11-16 11:55:13 +0000

    UI redesign for expertise module

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -9,6 +9,9 @@ export class PortfolioComponent implements OnInit {
   alphaAppDB= '../assets/images/Alphaapp dashboard.png ';
   salesUI = '../assets/images/SalesUI.png ';
   loginUIDesign = '../assets/images/Login UI design 1.png';
+ loginUIDesign2 = '../assets/images/Login UI design 2.png';
+healthMaxUIDesign = '../assets/images/Health Max Landing Page.png';
+healthMaxPackagesUI = '../assets/images/Health Max Packages.png';
 
   constructor() { }
 
