05a1454043a35dc5758142e6566c1e2c013c6523
ZAINABAFOLABI <zainab.o.afolabi@gmail.com>
2024-01-08 18:30:14 +0000

    API dev and integration UI, download UI setup

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'zainab-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  CV = '../assets/documents/Omolola Zainab Afolabi CV.pdf';
+
 
   constructor() { }
 
--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -34,6 +34,7 @@ sprint1 = 'I excellently champion sprint planning sessions.';
 sprint2 = 'I anchor daily stand up meetings to track team progress.';
 sprint3 = 'I supervise sprint demo days, accurately ensuring delivered product feature compliance with acceptance criteria and definition of done.';
 inputValidation = '../assets/video/compressed input validation.mp4';
+APIntegrationDevelopment =  '../assets/video/API dev and integration video.mp4';
 
   constructor() { }
 
